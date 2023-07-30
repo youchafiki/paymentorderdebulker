c12efce981fd81d399d8b3be4b1c70a177394b5c
youssef chafiki <youhafiki@gmail.com>
2023-07-30 22:53:51 +0200

    pain.001 debulker

---
+++ src/main/java/com/awb/payment/DebukerApplication.java
@@ -0,0 +1,13 @@
+package com.awb.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebukerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebukerApplication.class, args);
+	}
+
+}
